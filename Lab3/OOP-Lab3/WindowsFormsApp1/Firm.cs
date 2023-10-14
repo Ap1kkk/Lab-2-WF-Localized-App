b72f51c7118f0c9343a2beaacb367a3436501d42
Sergey Larshin <larshinsr@gmail.com>
2023-10-14 13:21:15 +0300

    Added View Project

---
+++ Lab3/OOP-Lab3/WindowsFormsApp1/Firm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class Firm
+    {
+        public string Name { get; private set; }
+        public string Country { get; private set; }
+        public string Region { get; private set; }
+        public string Town { get; private set; }
+        public string Street { get; private set; }
+        public string PostIndex { get; private set; }
+        public DateTime StartDate { get; private set; }
+        public string Email { get; private set; }
+        public string WebsiteUrl { get; private set; }
+
+        public Dictionary<string, string> UserFields { get; private set; }
+
+
+    }
+}
---
+++ Lab3/OOP-Lab3/WindowsFormsApp1/SubFirm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class SubFirm
+    {
+        public string Name { get; private set; }
+        public string BossName { get; private set; }
+        public string OfficialBossName { get; private set; }
+        public string PhoneNumber { get; private set; }
+        public string Email { get; private set; }
+        public SubFirmType Type { get; private set; }
+        public List<Contact> Contacts { get; private set; }
+    }
+}
